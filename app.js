62ed68f3b2b2ae2139475a0dee1279cc6fa7631e
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-01-26 11:18:21 +0530

    Setup mongoose connection

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
-const indexRouter = require("./routes/index-route");
+// Set up mongoose connection
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URI;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 const app = express();
 
@@ -18,8 +26,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
