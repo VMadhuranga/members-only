5ea73f165acce5e4a601689316bc8562e1fdc7ff
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-01 08:32:00 +0530

    Create createMessagePost and deleteMessagePost controllers
    Additional:
    - Populate user in messageListGet controller
    - Pass unescape function to message list view view

--- controllers/message-controller.js
+++ controllers/message-controller.js
@@ -1,16 +1,50 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const unescape = require("../utils/unescape");
 
 const MessageModel = require("../models/message-model");
 
 const messageListGet = asyncHandler(async (req, res, next) => {
-  const allMessages = await MessageModel.find({}).exec();
+  const allMessages = await MessageModel.find({}).populate("user").exec();
 
   res.render("message-list-view", {
     title: "Message List",
     messageList: allMessages,
+    unescape,
   });
+});
+
+const createMessagePost = [
+  body("message", "Message should not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const message = new MessageModel({
+      user: req.user.id,
+      message: req.body.message,
+    });
+
+    if (!errors.isEmpty()) {
+      req.app.locals.postError = errors.array();
+    } else {
+      await message.save();
+    }
+
+    res.redirect("/");
+  }),
+];
+
+const deleteMessagePost = asyncHandler(async (req, res, next) => {
+  await MessageModel.findByIdAndDelete(req.body["message-id"]);
+  res.redirect("/");
 });
 
 module.exports = {
   messageListGet,
+  createMessagePost,
+  deleteMessagePost,
 };
