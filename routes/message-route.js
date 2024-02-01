d8a53919cf612fbe94478761be34c032ac84b6ab
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-01 08:33:11 +0530

    Create routers to handle message create and delete POST requests

--- routes/message-route.js
+++ routes/message-route.js
@@ -5,5 +5,7 @@ const messageController = require("../controllers/message-controller");
 const router = express.Router();
 
 router.get("/", messageController.messageListGet);
+router.post("/create-message", messageController.createMessagePost);
+router.post("/delete-message", messageController.deleteMessagePost);
 
 module.exports = router;
